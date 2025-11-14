28e9d33fc310797c87ad9d01e92fe717209cfc8c
eaststone-ticketing <info@eaststone.se>
2025-11-14 14:21:53 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,72 +1,93 @@
+const API_URL = process.env.REACT_APP_API_URL || "http://192.168.8.171:5000";
+function getToken() { const user = localStorage.getItem('user'); return user ? JSON.parse(user).token : null; }
+
 export async function getKyrkogardar() {
-    const res = await fetch ("http://192.168.8.171:5000/kyrkogardar");
+    const res = await fetch (`${API_URL}/kyrkogardar`, {
+        headers: {"Content-Type": "application/json",
+                  "Authorization": `Bearer ${getToken()}`,
+                });
     return res.json();
 }
 
 export async function addKyrkogard(kyrkogard) {
-    const res = await fetch ("http://192.168.8.171:5000/kyrkogardar", {
+    const res = await fetch (`${API_URL}/kyrkogardar`, {
         method: "POST",
-        headers: {"Content-Type": "application/json"},
+        headers: {"Content-Type": "application/json",
+                  "Authorization": `Bearer ${getToken()}`} ,
         body: JSON.stringify(kyrkogard)
     });
     return res.json();
 }
 
 export async function removeKyrkogard(id) {
-    const res = await fetch (`http://192.168.8.171:5000/kyrkogardar/${id}`, {
+    const res = await fetch (`${API_URL}/kyrkogardar/${id}`, {
         method: "DELETE",
-        headers: {"Content-Type": "application/json"},
+        headers: {"Content-Type": "application/json",
+                  "Authorization": `Bearer ${getToken()}`,
     });
     return res.json(
     )
 }
 
 export async function getArenden() {
-    const res = await fetch ("http://192.168.8.171:5000/arenden")
+    const res = await fetch (`${API_URL}/arenden`, {
+        headers: {"Content-Type": "application/json",
+                  "Authorization": `Bearer ${getToken()}`,
+                });
     return res.json();
 }
 
 export async function addArende(arende) {
-    const res = await fetch ("http://192.168.8.171:5000/arenden", {
+    const res = await fetch (`${API_URL}/arenden`, {
         method: "POST",
-        headers: {"Content-Type": "application/json"},
+        headers: {"Content-Type": "application/json",
+                  "Authorization": `Bearer ${getToken()}`},
         body: JSON.stringify(arende)      
     });
     return res.json();
 }
 
 export async function removeArende(id) {
-    const res = await fetch (`http://192.168.8.171:5000/arenden/${id}`, {
+    const res = await fetch (`${API_URL}/arenden`, {
         method: "DELETE",
-        headers: {"Content-Type": "application/json"}
+        headers: {"Content-Type": "application/json",
+                  "Authorization": `Bearer ${getToken()}`
     });
     return res.json();
 }
 
 export async function getKunder() {
-    const res = await fetch ("http://192.168.8.171:5000/kunder")
+    const res = await fetch (`${API_URL}/kunder`, {
+        headers: {"Content-Type": "application/json",
+                  "Authorization": `Bearer ${getToken()}`,
+                });
     return res.json();
 }
 
 export async function addKund(kund) {
-    const res = await fetch ("http://192.168.8.171:5000/kunder", {
+    const res = await fetch (`${API_URL}/kunder`, {
         method: "POST",
-        headers: {"Content-Type": "application/json"},
+        headers: {"Content-Type": "application/json",
+                  "Authorization": `Bearer ${getToken()}`},
         body: JSON.stringify(kund)      
     });
     return res.json();
 }
 
 export async function removeKunder(id) {
-    const res = await fetch (`http://192.168.8.171:5000/kunder/${id}`, {
+    const res = await fetch (`${API_URL}/kunder/${id}`, {
         method: "DELETE",
-        headers: {"Content-Type": "application/json"}
+        headers: {"Content-Type": "application/json",
+                  "Authorization": `Bearer ${getToken()}`
     });
     return res.json();
 }
 
 export async function getGodkannanden() {
-    const res = await fetch ("http://192.168.8.171:5000/godkannanden");
+    const res = await fetch (`${API_URL}/godkannanden`, {
+        headers: {"Content-Type": "application/json",
+                  "Authorization": `Bearer ${getToken()}`},
+                });
 
     if (!res.ok) {
         console.log(`Error: ${res.status} - ${res.statusText}`);
@@ -77,24 +98,29 @@ export async function getGodkannanden() {
 }
 
 export async function addGodkannande(godkannande) {
-    const res = await fetch ("http://192.168.8.171:5000/godkannanden", {
+    const res = await fetch (`${API_URL}/godkannanden`, {
     method: "POST",
-    headers: {"Content-Type": "application/json"},
+    headers: {"Content-Type": "application/json",
+                  "Authorization": `Bearer ${getToken()}`},
     body: JSON.stringify(godkannande)      
     });
     return res.json();
 }
 
 export async function removeGodkannande(id) {
-    const res = await fetch (`http://192.168.8.171:5000/godkannanden/${id}`, {
+    const res = await fetch (`${API_URL}/godkannanden/${id}`, {
         method: "DELETE",
-        headers: {"Content-Type": "application/json"}
+        headers: {"Content-Type": "application/json",
+                  "Authorization": `Bearer ${getToken()}`
     });
     return res.json();
 }
 
 export async function getKommentarer() {
-    const res = await fetch ("http://192.168.8.171:5000/kommentarer");
+    const res = await fetch (`${API_URL}/kommentarer`, {
+        headers: {"Content-Type": "application/json",
+                  "Authorization": `Bearer ${getToken()}`},
+                 });
 
     if (!res.ok) {
         console.log(`Error: ${res.status} - ${res.statusText}`);
@@ -105,9 +131,10 @@ export async function getKommentarer() {
 
 export async function addKommentarer(kommentar) {
   try {
-    const res = await fetch("http://192.168.8.171:5000/kommentarer", {
+    const res = await fetch(`${API_URL}/kommentarer`, {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
+      headers: { "Content-Type": "application/json",
+                  "Authorization": `Bearer ${getToken()}`},
       body: JSON.stringify(kommentar),
     });
 
@@ -125,54 +152,61 @@ export async function addKommentarer(kommentar) {
 
 
 export async function removeKommentarer(id) {
-    const res = await fetch (`http://192.168.8.171:5000/kommentarer/${id}`, {
+    const res = await fetch (`${API_URL}/kommentarer/${id}`, {
         method: "DELETE",
-        hearders: {"Content-Type": "application/json"}
+        headers: {"Content-Type": "application/json",
+                  "Authorization": `Bearer ${getToken()}`
     });
     return res.json();
 }
 
 export async function updateGodkannande(id,data) {
-    const res = await fetch(`http://192.168.8.171:5000/godkannanden/${id}`, {
+    const res = await fetch(`${API_URL}/godkannanden/${id}`, {
     method: "PUT",
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json",
+                  "Authorization": `Bearer ${getToken()}`},
     body: JSON.stringify(data)
   });
   return res.json();
 }
 
 export async function updateKyrkogard(id, data) {
-  const res = await fetch(`http://192.168.8.171:5000/kyrkogardar/${id}`, {
+  const res = await fetch(`${API_URL}/kyrkogardar/${id}`, {
     method: "PUT",
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json",
+                  "Authorization": `Bearer ${getToken()}`} ,
     body: JSON.stringify(data)
   });
   return res.json();
 }
 
 export async function updateArende(id, data) {
-  const res = await fetch(`http://192.168.8.171:5000/arenden/${id}`, {
+  const res = await fetch(`${API_URL}/arenden/${id}`, {
     method: "PUT",
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json",
+                  "Authorization": `Bearer ${getToken()}`} ,
     body: JSON.stringify(data)
   });
   return res.json();
 }
 
 export async function updateKund(id, data) {
-  const res = await fetch(`http://192.168.8.171:5000/kunder/${id}`, {
+  const res = await fetch(`${API_URL}/kunder/${id}`, {
     method: "PUT",
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json",
+                  "Authorization": `Bearer ${getToken()}`} ,
     body: JSON.stringify(data)
   });
   return res.json();
 }
 
 export async function updateKommentar(id, data) {
-  const res = await fetch(`http://192.168.8.171:5000/kommentarer/${id}`, {
+  const res = await fetch(`${API_URL}/kommentarer/${id}`, {
     method: "PUT",
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json",
+                  "Authorization": `Bearer ${getToken()} `},
     body: JSON.stringify(data)
   });
   return res.json();
 }
+
