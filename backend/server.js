8594f4cc9804c027d46ce760898f88857cce0648
Felix <info@eaststone.se>
2025-11-14 17:43:56 +0100

    omg

--- backend/server.js
+++ backend/server.js
@@ -453,5 +453,5 @@ app.put("/kommentarer/:id", authenticateToken, async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(5000, () => console.log(`Server running on port ${PORT}`));
 
