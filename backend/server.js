d7a209c27e9c3ba1e02c21d228b7d60a29d29828
Felix <info@eaststone.se>
2025-11-20 10:24:22 +0100

    -

--- backend/server.js
+++ backend/server.js
@@ -562,7 +562,7 @@ app.post("/refresh-token", async (req, res) => {
     });
 });
 
-app.get("/alter-table", async (req,res) => {
+app.get("/alter-table", authenticateToken, async (req,res) => {
 
   const alterTableSQL = 'ALTER TABLE arenden ADD COLUMN deleted_at TIMESTAMP DEFAULT NULL';
 
