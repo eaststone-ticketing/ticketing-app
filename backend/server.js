aef229dd74dd0f68528e779195281e9a5f439666
Felix <info@eaststone.se>
2026-02-09 11:46:54 +0100

    -

--- backend/server.js
+++ backend/server.js
@@ -740,7 +740,7 @@ app.get("/arendepdf/:arendeId", authenticateToken, async (req, res) => {
       formName = "form.pdf"
       console.log(arende.kyrkogard);
     } else if (kyrkogardar_stockholm.includes(arende.kyrkogard) && arende.arendeTyp === "Ny sten"){
-      formName = "formsthlm.pdf"
+      formName = "formsthlm.pdf" 
       console.log(arende.kyrkogard);
     } else
       {
