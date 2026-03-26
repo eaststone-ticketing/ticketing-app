3c75c7132d5a6198ea77a0532525c1fdc7081134
Felix <info@eaststone.se>
2026-03-26 14:15:29 +0100

    -

--- backend/server.js
+++ backend/server.js
@@ -980,7 +980,7 @@ app.post("/refresh-token", async (req, res) => {
     });
 });
 
-app.get("/alter-table", authenticateToken, async (req,res) => {
+app.get("/alter-table", async (req,res) => {
 
   const alterTableSQL = `
     ALTER TABLE traces
